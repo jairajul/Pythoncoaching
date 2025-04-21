921f25b370143d2ceec5f2a133a85606ceb7edb4
jairajul <rajulgpt02@gmail.com>
2025-04-21 19:24:09 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,17 @@
+#Task 1: Perform Basic Mathematical Operations
+a=input('Enter the first value: ')
+b=input('Enter the second value: ')
+a=int(a)
+b=int(b)
+print('Addition: ',a+b)
+print('Subtraction: ',a-b)
+print('Multiplication: ',a*b)
+print('Division: ',a/b)
+
+
+#Task 2: Create a Personalized Greeting
+x=input('Enter your first name: ')
+y=input('Enter your last name: ')
+z=f"{x} {y}"
+s=f"Hello, {z}! Welcome to the Python program"
+print(s)
